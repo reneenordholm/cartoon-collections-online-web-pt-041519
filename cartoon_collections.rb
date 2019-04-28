beed21f105d4370771873b65b60b1ed6f44d8b3a
Renee Nordholm <reneenordholm@gmail.com>
2019-04-28 01:07:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def long_planeteer_calls(calls_long)
 end
 
 def find_the_cheese(contains_cheddar)
-  
   cheese_types = ["cheddar", "gouda", "camembert"]
+  contains_cheddar.include?(cheese_types ? cheese_types : nil)
 end
