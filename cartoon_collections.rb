a74edca4d2bcf8c97dba2b8234d9ac973168e6f4
Renee Nordholm <reneenordholm@gmail.com>
2019-04-28 00:55:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index { |dwarves, index| puts "#{index+1} #{dwarves}" }  
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(veggies)
   # Your code here
 end
 
