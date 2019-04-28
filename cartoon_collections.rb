22332467aed5ba342c83b47ca683254dd269cf57
Renee Nordholm <reneenordholm@gmail.com>
2019-04-28 01:04:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(calls_long)
   calls_long.any? { |calls| calls.length > 4 ? true : false }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(contains_cheddar)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
