932d1851e76dfa4554f867b01e9147c787599007
Renee Nordholm <reneenordholm@gmail.com>
2019-04-28 01:28:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ end
 
 def find_the_cheese(contains_cheddar)
   cheese_types = ["cheddar", "gouda", "camembert"]
-    contains_cheddar.each do |cheese| 
+    contains_cheddar.map.join do |cheese| 
     if cheese.include?(cheese_types)
      puts cheese_types(cheese)
     else
