d9e53a5edfc0a353774fb1bb47e487a634c79862
Renee Nordholm <reneenordholm@gmail.com>
2019-04-28 01:19:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,9 +13,6 @@ end
 
 def find_the_cheese(contains_cheddar)
   cheese_types = ["cheddar", "gouda", "camembert"]
-    if contains_cheddar.include?(cheese_types)
-      return contains_cheddar.select
-    else
-      nil
-  end
+  contains_chedder.map 
+    { |cheese| cheese.include?(cheese_types) ? cheese_types(cheese) : nil }
 end
